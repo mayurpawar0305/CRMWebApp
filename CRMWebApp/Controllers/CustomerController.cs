c208221c149f21f1c04c99eb69ebdb229bd53714
mayurpawar0305 <mayurpawar0305@gmail.com>
2022-07-16 16:08:47 +0530

    customer controller is added

---
+++ CRMWebApp/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CRMWebApp.Models;
+
+namespace CRMWebApp.Controllers
+{
+    //customer/index
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new Customer() { Id = 23, Name = "Mayur", ContactNo = "9552030714" });
+            customers.Add(new Customer() { Id = 24, Name = "Gaurav", ContactNo = "364897522" });
+            customers.Add(new Customer() { Id = 25, Name = "Yash", ContactNo = "2156485456" });
+            customers.Add(new Customer() { Id = 26, Name = "Rajesh", ContactNo = "9878421657" });
+            customers.Add(new Customer() { Id = 27, Name = "Yogesh", ContactNo = "7654314788" });
+            customers.Add(new Customer() { Id = 28, Name = "Atharva", ContactNo = "13468768" });
+
+            return View(customers);
+        }
+    }
+}
--- CRMWebApp/Controllers/HomeController.cs
+++ CRMWebApp/Controllers/HomeController.cs
@@ -7,21 +7,14 @@ using System.Web.Mvc; //Models has been use
 using CRMWebApp.Models;
 namespace CRMWebApp.Controllers
 {
+    
     public class HomeController : Controller
     {
         // GET: Home
         //List of Action Method
         public ActionResult Index()
-        {
-            List<Customer> customers = new List<Customer>();
-            customers.Add(new Customer() { Id = 23, Name = "Mayur", ContactNo = "9552030714"});
-            customers.Add(new Customer() { Id = 24, Name = "Gaurav", ContactNo = "364897522" });
-            customers.Add(new Customer() { Id = 25, Name = "Yash", ContactNo = "2156485456" });
-            customers.Add(new Customer() { Id = 26, Name = "Rajesh", ContactNo = "9878421657" });
-            customers.Add(new Customer() { Id = 27, Name = "Yogesh", ContactNo = "7654314788" });
-
-            return this.Json(customers,JsonRequestBehavior.AllowGet);
-            //return View();
+        {            
+            return View();
         }
         public ActionResult AboutUs()
         {
